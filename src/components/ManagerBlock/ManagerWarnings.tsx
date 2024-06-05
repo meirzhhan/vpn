ce6f6a2c8b6818d90539c1fa4fe82521cc63103b
TynysA <jjakellucas@gmail.com>
2024-06-05 19:08:41 +0500

    add ai

--- src/components/ManagerBlock/ManagerWarnings.tsx
+++ src/components/ManagerBlock/ManagerWarnings.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import cl from './Manager.module.scss';
-
+import React from "react";
+import cl from "./Manager.module.scss";
+import ai from "../../db/ai.json";
 const ManagerWarnings = () => {
-  return <div className={cl.manager}></div>;
+  return (
+    <div className={cl.manager}>
+      {ai.map(({ text }, index) => (
+        <div ai="alert alert-ai">{ text}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ManagerWarnings;
